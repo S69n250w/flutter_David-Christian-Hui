483470cb15ae04378c566b4a5a2b99f237dacf1e
David C <211112742@students.mikroskil.ac.id>
2023-09-19 23:07:58 +0700

    Update tugas Introduction Flutter Widget
    menambahkan file tugas tentang Introduction Flutter Widget

--- 08_Introduction Flutter Widget/praktikum/latihan/david_c/test/widget_test.dart
+++ 08_Introduction Flutter Widget/praktikum/latihan/david_c/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:david_c/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyNama());
+    await tester.pumpWidget(const MyName());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
---
+++ 08_Introduction Flutter Widget/praktikum/tes/tes_1/android/app/src/main/kotlin/com/example/tes_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tes_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
