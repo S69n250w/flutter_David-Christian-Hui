8306f07b070a6ad504df264c95a63d60bcc310d6
David C <211112742@students.mikroskil.ac.id>
2023-10-26 02:18:01 +0700

    Update tugas Mini Project
    menambahkan fitur-fiturnya

---
+++ 03_Fundamental Dart/praktikum/Soal_Eksplorasi.dart
@@ -0,0 +1,7 @@
+// import 'dart:io';
+
+void palindrom() {
+  print("Soal 1:");
+  List<String> hurufAbjadKecil = [];
+  
+  // while (true) {
--- Mini Project/praktikum/david_c_mini/lib/home_page.dart
+++ Mini Project/praktikum/david_c_mini/lib/home_page.dart
@@ -11,13 +11,73 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final Color selectedColor = Colors.tealAccent;
+    final Color unselectedColor = Colors.grey;
+
+    final List<String> imagePaths = [
+      'assets/ayam_asam_manis.jpg',
+      'assets/ayam_kemangi.jpg',
+      'assets/ayam_lada_hitam.jpg',
+      'assets/ayam_obat.jpg',
+      'assets/ayam_tumis_jamur_kuping.jpg',
+      'assets/brokoli_saus_jamur.jpg'
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Daily Fresh"),
         centerTitle: true,
       ),
-      // body: GridView(gridDelegate: gridDelegate),
-
+      body: ListView.builder(
+        itemCount: imagePaths.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: [
+              Container(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (_) => const MenuPage()));
+                  },
+                  child: Image(
+                      width: 139,
+                      image: AssetImage(imagePaths[index]),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+        // children: [
+          // Row(
+          //   children: [
+          //     Container(
+          //       child: ElevatedButton(
+          //         onPressed: () {
+          //           Navigator.of(context).push(
+          //               MaterialPageRoute(builder: (_) => const MenuPage()));
+          //         },
+          //         child: Image(
+          //             width: 139,
+          //             image: AssetImage('assets/ayam_asam_manis.jpg')
+          //         ),
+          //       ),
+          //     )
+          //   ],
+          // )
+        // ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[850],
+        selectedItemColor: selectedColor,
+        unselectedItemColor: unselectedColor,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
+          BottomNavigationBarItem(icon: Icon(Icons.payment), label: 'Order'),
+          BottomNavigationBarItem(icon: Icon(Icons.usb), label: 'Profile'),
+        ]
+      ),
       // body: Center(
       //   child: ElevatedButton(
       //     onPressed: () {
