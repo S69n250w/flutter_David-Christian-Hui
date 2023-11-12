8b507578a6153ca21afc9fe39bf4ebf08cfd4e03
David C <211112742@students.mikroskil.ac.id>
2023-11-13 04:32:14 +0700

    tugas onboarding2
    menambahkan fitur

---
+++ coba/percobaan1/lib/onboarding2_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen2(),
+    ));
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  _SplashScreen2State createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => SplashScreen2(),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
