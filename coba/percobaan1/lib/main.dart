5565f226f5fd82e868cce4e718c8f9cfc44d31ec
David C <211112742@students.mikroskil.ac.id>
2023-11-21 08:24:29 +0700

    Update tugas Capstone
    membuat background

--- coba/percobaan1/lib/main.dart
+++ coba/percobaan1/lib/main.dart
@@ -1,25 +1,109 @@
-import 'package:flutter/material.dart';
-import 'package:percobaan1/onboarding2_page.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() => runApp(MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: OnBoarding2Page(),
+//     ));
+
+// class OnBoarding2Page extends StatefulWidget {
+//   const OnBoarding2Page({super.key});
+
+//   @override
+//   _OnBoarding2PageState createState() => _OnBoarding2PageState();
+// }
+
+// class _OnBoarding2PageState extends State<OnBoarding2Page> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Future.delayed(Duration(seconds: 2), () {
+//       Navigator.of(context).pushReplacement(MaterialPageRoute(
+//         builder: (context) => OnBoarding2Page(),
+//       ));
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage("assets/images/onboarding_page.png"),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnBoarding2Page(),
+    ));
+
+class OnBoarding2Page extends StatefulWidget {
+  const OnBoarding2Page({super.key});
+
+  @override
+  State<OnBoarding2Page> createState() => _OnBoarding2PageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _OnBoarding2PageState extends State<OnBoarding2Page> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background Image with BlendMode
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/onboarding_page.png'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.7),
+                  BlendMode.dstATop
+                ),
+              ),
+            ),
+          ),
+          // Linear Gradient Overlay for Top
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white,
+                  Colors.black.withOpacity(0.7),
+                ],
+                stops: [0.0, 0.8],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              ),
+            ),
+          ),
+          // Linear Gradient Overlay for Bottom
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.transparent,
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.7)
+                ],
+                stops: [0.0, 0.7, 1.0],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              ),
+            ),
+          ),
+        ],
       ),
-      home: const OnBoarding2Page(),
     );
   }
 }
