3e2cedcb7e4a876adafeb14a60a19a3a41c9c1bb
David C <211112742@students.mikroskil.ac.id>
2023-12-26 13:42:24 +0700

    Update tugas Flutter Form
    mengumpulkan tugas tentang Flutter Form

--- 13_Flutter Form/praktikum/soal_prioritas_1/lib/main.dart
+++ 13_Flutter Form/praktikum/soal_prioritas_1/lib/main.dart
@@ -1,3 +1,5 @@
+// sudah dengan Soal Prioritas 2 dan Soal Eksplorasi
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,6 +32,78 @@ class _ContactsPageState extends State<ContactsPage> {
     _nameController.dispose();
     _phoneController.dispose();
     super.dispose();
+  }
+
+  void _deleteContact(int index) {
+    print('Delete: ${contacts[index]['title']!}');
+    setState(() {
+      contacts.removeAt(index);
+    });
+  }
+
+  void _editContact(int index) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController phoneController = TextEditingController();
+
+    //Inisialisasi controller dengan nilai yang ada
+    nameController.text = contacts[index]['title']!;
+    phoneController.text = contacts[index]['subtitle']!;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Contact'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'New Name'
+                  ),
+                ),
+                TextFormField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    labelText: 'New Phone Number'
+                  ),
+                )
+              ],
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cancel')
+                ),
+                SizedBox(width: 16),
+                TextButton(
+                  onPressed: () {
+                    // Lakukan sesutau dengan nilai yang telah diubah
+                    setState(() {
+                      // Update nilai di dalam list contacts
+                      contacts[index]['title'] = nameController.text;
+                      contacts[index]['subtitle'] = phoneController.text;
+
+                      //Tutup dialog
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Text('Save'),
+                ),
+              ],
+            ),
+          ],
+        );
+      }
+    );
+    print('Edit: ${contacts[index]['title']!}');
   }
 
   @override
@@ -84,7 +158,11 @@ class _ContactsPageState extends State<ContactsPage> {
                               fontSize: 20, fontWeight: FontWeight.w500),
                         )),
                         SizedBox(height: 20),
-                        ContactsListWidget(contacts: contacts),
+                        ContactsListWidget(
+                          contacts: contacts,
+                          onDelete: _deleteContact,
+                          onEdit: _editContact,
+                        ),
                       ],
                     ),
                   ),
@@ -101,9 +179,28 @@ class _ContactsPageState extends State<ContactsPage> {
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
+                        return 'Nama harus diisi';
                       }
-                      return null;
+                      
+                      // Memastikan nama terdiri dari minimal 2 kata
+                      List<String> words = value.split(' ');
+                      if (words.length < 2) {
+                        return 'Nama harus terdiri dari minimal 2 kata';
+                      }
+
+                      // Memastikan setiap kata dimulai dengan huruf kapital
+                      for (String word in words) {
+                        if (!RegExp(r'^[A-Z][a-z]*$').hasMatch(word)) {
+                          return 'Setiap kata harus dimulai dengan huruf kapital';
+                        }
+                      }
+
+                      // Memastikan nama tidak mengandung angka atau karakter khusus
+                      if (RegExp(r'[0-9~`!@#%^&*()-+_=,.?{}<>/\;:"[]|]').hasMatch(value)) {
+                        return'Nama tidak boleh mengandung angka atau karakter';
+                      }
+
+                      return null; // Validasi berhasil
                     },
                   ),
                   SizedBox(height: 10),
@@ -116,8 +213,24 @@ class _ContactsPageState extends State<ContactsPage> {
                         labelStyle: TextStyle(fontWeight: FontWeight.w500)),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
+                        return 'Nomor telepon harus diisi';
                       }
+
+                      // Memastikan nomor telepon hanya terdiri dari angka saja
+                      if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                        return 'Nomor telepon harus terdiri dari angka saja';
+                      }
+
+                      // Memastikan panjang nomor telepon minimal 8 digit dan maksimal 15 digit
+                      if (value.length < 8 || value.length > 15) {
+                        return 'Panjang nomor telepon harus minimal 8 digit dan maksimal 15 digit';
+                      }
+
+                      //Memastikan nomor telepon dimulai dengan angka 0
+                      if (!RegExp(r'^0').hasMatch(value)) {
+                        return 'Nomor telepon harus dimulai dengan angka 0';
+                      }
+
                       return null;
                     },
                   ),
@@ -143,7 +256,7 @@ class _ContactsPageState extends State<ContactsPage> {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               content: Text(
-                                  'New Contact: ${_nameController.text}, ${_phoneController.text}'),
+                                'New Contact: ${_nameController.text}, ${_phoneController.text}'),
                             ),
                           );
                         }
@@ -179,8 +292,10 @@ class _ContactsPageState extends State<ContactsPage> {
 
 class ContactsListWidget extends StatelessWidget {
   final List<Map<String, String>> contacts;
+  final Function(int) onDelete;
+  final Function(int) onEdit;
 
-  ContactsListWidget({required this.contacts});
+  ContactsListWidget({required this.contacts, required this.onDelete, required this.onEdit});
 
   @override
   Widget build(BuildContext context) {
@@ -208,25 +323,35 @@ class ContactsListWidget extends StatelessWidget {
               leading: CircleAvatar(
                 backgroundColor: Colors.purple[100],
                 child: Text(
-                  contacts[index]['title']![0].toUpperCase(),
+                  '${contacts[index]['title']![0].toUpperCase()}',
                   style: TextStyle(
                     color: Colors.black
                   ),
                 ),
               ),
-              title: Text(contacts[index]['title']!.toUpperCase()),
-              subtitle: Text(contacts[index]['subtitle']!),
+              title: Text(
+                '${capitalizeWords(contacts[index]['title']!)}'
+                // '${contacts[index]['title']!}'
+              ),
+              subtitle: Text(
+                '${contacts[index]['subtitle']!}'
+              ),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   IconButton(
                     onPressed: () {
-                      print('Edit: ${contacts[index]['title']}');
+                      if (index < contacts.length) {
+                        // Panggil fungsi onEdit saat tombol edit ditekan
+                        onEdit(index);
+                      }
                     }, 
                     icon: Icon(Icons.mode_edit_outlined)
                   ),
-                  IconButton(onPressed: () {
-                    print('Delete: ${contacts[index]['title']}');
+                  IconButton(
+                    onPressed: () {
+                      // Panggil fungsi onDelete saat tombol delete ditekan
+                      onDelete(index);
                   },
                   icon: Icon(Icons.delete_outline_outlined)
                   ),
@@ -238,4 +363,25 @@ class ContactsListWidget extends StatelessWidget {
       ],
     );
   }
+}
+
+// Fungsi untuk mengonversi teks
+String capitalizeWords(String input) {
+  if (input.isEmpty) {
+    return input;
+  }
+
+  // Pisahkan input menjadi kata-kata
+  List<String> words = input.split(' ');
+
+  // Ubah setiap kata menjadi huruf kapital awal dan sisanya huruf kecil
+  List<String> capitalizeWords = words.map((word) {
+    if (word.isEmpty) {
+      return word; // Biarkan string kosong jika ada
+    }
+    return '${word[0].toUpperCase()}${word.substring(1).toLowerCase()}';
+  }).toList();
+
+  // Gabungkan kata-kata yang telah diubah kembali mejadi satu string
+  return capitalizeWords.join(' ');
 }
