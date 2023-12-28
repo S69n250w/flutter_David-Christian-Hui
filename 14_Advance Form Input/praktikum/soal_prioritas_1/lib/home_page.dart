613d5c84abe40ef35ebff7a1cac9405e33cd0e65
David C <211112742@students.mikroskil.ac.id>
2023-12-28 10:53:40 +0700

    Update tugas Advance Form Input
    mengumpulkan tugas tentang Advance Form Input

---
+++ 14_Advance Form Input/praktikum/soal_prioritas_1/lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late DateTime selectedDate;
+  late Color selectedColor;
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
